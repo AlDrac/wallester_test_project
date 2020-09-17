56a5d80f029dd6154ee4429cc97f726e5245833c
Sergey Saprykin <sergey.saprykin@placetgroup.com>
2020-09-17 19:14:52 +0300

    fix interface

--- app/api/repositories/customer_repository.go
+++ app/api/repositories/customer_repository.go
@@ -1,13 +1,14 @@
 package repositories
 
 import (
-	"net/url"
-
 	"github.com/AlDrac/wallister_test_project/app/api/models"
+	"net/url"
 )
 
 type CustomerRepository interface {
 	Create(customer *models.Customer) error
-	GetById(int) (*models.Customer, error)
+	Edit(customer *models.Customer) error
+	Delete(int) error
 	Get(values url.Values) ([]models.Customer, error)
+	GetById(int) (*models.Customer, error)
 }
